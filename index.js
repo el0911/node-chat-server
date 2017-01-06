4ae3605c3ac509dc10aa6baede4606ebacf55b6c
somto <somto.achu5@gmail.com>
2017-01-06 20:19:02 +0100

    fix

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ console.log("Listening on port " + PORT);
         socket.on('update', function(data){
             MongoClient.connect(url, function(err, db){
               var collection = db.collection('documents');
-              collection.find({receiver:x,status:1}).toArray(function(err, docs){
+              collection.find({receiver:data.id,status:1}).toArray(function(err, docs){
                 send2(docs);
                 db.close();
               });
